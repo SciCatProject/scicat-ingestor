8f0d6d6f195c7dbf8b81dbbe1b018417742df435
YooSunyoung <luysunyoung9@gmail.com>
2024-04-05 12:20:05 +0200

    Minimal structure of the codebase.

---
+++ src/scicat_filewriter_ingest.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 ScicatProject contributors (https://github.com/ScicatProject)
+
+
+def main() -> None:
+    """Main entry point of the app."""
+    ...
