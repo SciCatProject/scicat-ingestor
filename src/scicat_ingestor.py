851159707cd8c37bd03a14f897861aa55902b8f8
YooSunyoung <luysunyoung9@gmail.com>
2024-06-14 03:29:51 +0200

    Add versioning.

--- src/scicat_ingestor.py
+++ src/scicat_ingestor.py
@@ -1,5 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2024 ScicatProject contributors (https://github.com/ScicatProject)
+# Copyright (c) 2024 Scicatproject contributors (https://github.com/ScicatProject)
+# ruff: noqa: E402, F401
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+del importlib
+
 import logging
 from collections.abc import Generator
 from contextlib import contextmanager
