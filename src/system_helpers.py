b06500d1e42d1c19bd005606270eba508cfd08e3
YooSunyoung <luysunyoung9@gmail.com>
2024-08-08 09:16:28 +0200

    Add docstring of the helpers.

--- src/system_helpers.py
+++ src/system_helpers.py
@@ -20,7 +20,36 @@ def handle_daemon_loop_exceptions(
     safe_exit_type: tuple[type[BaseException], ...] = (KeyboardInterrupt,),
     ignored_exceptions: tuple[type[BaseException], ...] = (),
 ) -> Generator[None, None, None]:
-    """Exit the program if an exception is raised."""
+    """Exit the program if an exception is raised.
+    This context manager is used in the main loop of the daemon.
+
+    <If an exception is raised>
+    If the exception is in the ``safe_exit_type`` tuple,
+    the program exits without printing the traceback.
+
+    If the exception is in the ``ignored_exceptions`` tuple,
+    the program logs the exception and continues the loop.
+
+    If the exception is not in the above two tuples,
+    the program exits and prints the traceback of the exception.
+
+    <If there is no exception>
+    Daemon is not expected to exit even if there is *NO* exception.
+    Therefore ``else`` block is used to exit the program
+    if the loop finishes unexpectedly.
+
+    Params
+    ------
+    logger:
+        The logger object.
+    safe_exit_type:
+        The tuple of exceptions that should not print the traceback.
+        Default is ``(KeyboardInterrupt,)``.
+    ignored_exceptions:
+        The tuple of exceptions that should be ignored.
+        Default is an empty tuple.
+
+    """
     try:
         yield
     except safe_exit_type:
@@ -45,7 +74,11 @@ def handle_daemon_loop_exceptions(
 def handle_exceptions(
     logger: logging.Logger,
 ) -> Generator[None, None, None]:
-    "Exit the program if an exception is raised."
+    """Exit the program if an exception is raised.
+
+    If an exception is raised, the program logs the error and exits with status code 1.
+    If no exception is raised, the program exits with status code 0.
+    """
     try:
         yield
     except Exception as e:
