2cb5137ea7623575bc31cd716a6e137b3fd3c31b
YooSunyoung <luysunyoung9@gmail.com>
2024-10-21 15:32:05 +0200

    Hide api url from argument and expose api-endpoints instead.

--- src/scicat_configuration.py
+++ src/scicat_configuration.py
@@ -231,6 +231,22 @@ class DatasetOptions:
 
 
 @dataclass(kw_only=True)
+class _ScicatAPIURLs:
+    datasets: str
+    proposals: str
+    origdatablocks: str
+    instruments: str
+
+
+@dataclass(kw_only=True)
+class ScicatEndpoints:
+    datasets: str = "datasets"
+    proposals: str = "proposals"
+    origdatablocks: str = "origdatablocks"
+    instruments: str = "instruments"
+
+
+@dataclass(kw_only=True)
 class SciCatOptions:
     host: str = "https://scicat.host"
     token: str = "JWT_TOKEN"
@@ -238,7 +254,16 @@ class SciCatOptions:
     timeout: int = 0
     stream: bool = True
     verify: bool = False
-    urls: dict = field(default_factory=dict)
+    api_endpoints: ScicatEndpoints = field(default_factory=ScicatEndpoints)
+
+    @property
+    def urls(self) -> _ScicatAPIURLs:
+        return _ScicatAPIURLs(
+            datasets=urljoin(self.host, self.api_endpoints.datasets),
+            proposals=urljoin(self.host, self.api_endpoints.proposals),
+            origdatablocks=urljoin(self.host, self.api_endpoints.origdatablocks),
+            instruments=urljoin(self.host, self.api_endpoints.instruments),
+        )
 
     @classmethod
     def from_configurations(cls, config: dict) -> "SciCatOptions":
@@ -248,12 +273,6 @@ class SciCatOptions:
         options.headers = {
             **options.headers,
             **{"Authorization": f"Bearer {options.token}"},
-        }
-        options.urls = {
-            "datasets": urljoin(options.host, "datasets"),
-            "proposals": urljoin(options.host, "proposals"),
-            "origdatablocks": urljoin(options.host, "origdatablocks"),
-            "instruments": urljoin(options.host, "instruments"),
         }
         return options
 
